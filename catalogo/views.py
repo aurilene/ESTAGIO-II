89a86d78958fdf8dfeb02d23c32faf11401457e2
auri <auritp19@gmail.com>
2018-09-02 23:31:43 -0300

    alterações no produto

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+
+def product_list(request):
+    context = {
+        'products': Product.objects.all()
+    }
+    return render(request,'catalogo/product_list.html',context)
--- core/views.py
+++ core/views.py
@@ -10,14 +10,11 @@ from catalogo.models import Category
 def index(request):
 	context = {
 		'categories': Category.objects.all()
-	}	
+	}
 	return render(request,'index.html',context)
 
 def contact(request):
 	return render(request,'contact.html')
-
-def product_list(request):
-	return render(request,'product_list.html')
 
 def product(request):
 	return render(request,'product.html')
